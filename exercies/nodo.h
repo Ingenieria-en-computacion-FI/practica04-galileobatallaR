b1b6c01acb611526d1a95290b3526bb909bbbbb3
galileobatallaR <galileo1324@hotmail.com>
2026-03-12 23:20:02 -0600

    Update nodo.h

--- exercies/nodo.h
+++ exercies/nodo.h
@@ -8,12 +8,12 @@ typedef struct Nodo {
     struct Nodo* siguiente;
 } Nodo;
 
-Nodo* crearNodo(int v);
-Nodo* crearArregloNodos(int cant);
-Nodo* agregarNodo(Nodo* ptr_base, int* size, int v);
-void liberarNodos(Nodo* ptr, int n);
+Nodo* crearNodo(int valor);
+Nodo* crearArregloNodos(int n);
+Nodo* agregarNodo(Nodo* arreglo, int* n, int valor);
+void liberarNodos(Nodo* arreglo, int n);
 Nodo* construirTresNodos();
-int contarNodos(Nodo* head);
+int contarNodos(Nodo* inicio);
 Nodo* crearNodosPorEntrada();
 
 #endif
